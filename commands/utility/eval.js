d2813eb8ca9545ea443b16ae961f0d3a3125736e
Luca Biagetti <notciabaku@gmail.com>
2020-10-31 22:15:19 +0100

    BOT - eval command, adding arguments limitations.

--- commands/utility/eval.js
+++ commands/utility/eval.js
@@ -2,10 +2,11 @@ const { ownerID } = require('../../config.json');
 const { permissions } = require('./status');
 module.exports = {
     commands: 'eval',
-    minArgs: 0,
+    minArgs: 1,
+    maxArgs: 1,
     callback: (message, arguments, text, client) => {
         if(message.member.id === ownerID){
-            let result = eval(text);
+            let result = eval(arguments);
             message.reply(result);
         }
     },
