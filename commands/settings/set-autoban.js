0e0c8798547ad466cd0a8f7d2c92f2bf6abedcbc
Luca Biagetti <notciabaku@gmail.com>
2020-11-01 23:40:39 +0100

    BOT - set-autoban command.

---
+++ commands/settings/set-autoban.js
@@ -0,0 +1,29 @@
+const serverSettingsSchema = require('@schemas/server-settings-schema');
+
+module.exports = {
+    commands: 'setautoban',
+    expectedArgs: '<enabled/disabled>',
+    minArgs: 1,
+    maxArgs: 1,
+    callback: async (message, arguments, text, client) => {
+        const guild = message.guild.id;
+        const state = () => {
+            if (arguments[0] === 'enabled') return true;
+            if (arguments[0] === 'disabled') return false;
+        }
+
+        await serverSettingsSchema.findByIdAndUpdate({
+            _id: guild
+        }, {
+            features: {
+                autoban: state()
+            }
+        }, {
+            upsert: true
+        });
+
+        message.channel.send(`Autoban feature state: \`${arguments[0]}\``);
+    },
+    permissions: 'ADMINISTRATOR',
+    requiredRoles: [],
+}
