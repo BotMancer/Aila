ec42c7e80d464f26c51059fe5963896502c71a9e
Luca Biagetti <notciabaku@gmail.com>
2020-11-06 20:11:11 +0100

    CodeRef - commands/test section.

--- commands/test/dm.js
+++ commands/test/dm.js
@@ -1,9 +1,11 @@
+function DiffDays(creationDate, todayDate) {
+    var timeDiff = todayDate - creationDate;
+    return Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+};
+
 module.exports = {
     commands: ['dm', 'test'],
-    expectedArgs: '',
-    permissionError: 'non hai i permessi necessari per eseguire questo comando.',
-    minArgs: 0,
-    maxArgs: null,
+    maxArgs: 0,
     callback: (message, arguments, text, client) => {
         message.delete();
         //TEST SECTION//
@@ -12,11 +14,7 @@ module.exports = {
         todayDate = (todayDate.getMonth() + 1) + '/' + todayDate.getDate() + '/' + todayDate.getFullYear();
         const memberCreationDate = new Intl.DateTimeFormat('en-US').format(member.createdAt);
         const memberAge = DiffDays(Date.parse(memberCreationDate), Date.parse(todayDate));
-        function DiffDays(creationDate, todayDate) {
-            var timeDiff = todayDate - creationDate;
-            return Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-        };
-        // --------- //
+        // ---------- //
         const authorID = message.author.id;
         client.users.fetch(authorID).then((user) => {
             user.send(`Questo è un messaggio autogenerato dal sistema.\n${memberAge}\n${memberCreationDate}\n${todayDate}`);
