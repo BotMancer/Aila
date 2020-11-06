cc3c05dc4a0e5de3f41bf94c120da1e5c454a49f
Luca Biagetti <notciabaku@gmail.com>
2020-11-06 23:48:21 +0100

    CodeRef - features/security section.

--- features/security/autoban.js
+++ features/security/autoban.js
@@ -18,28 +18,30 @@ module.exports = (client, guild) => {
         if (guild == member.guild) {
             //Getting state from DB.
             const { state, log_channel } = await checkState(member.guild.id);
+
             //Checking state and if true ban user.
             if (state) {
                 var todayDate = new Date();
                 todayDate = (todayDate.getMonth() + 1) + '/' + todayDate.getDate() + '/' + todayDate.getFullYear();
                 const memberCreationDate = new Intl.DateTimeFormat('en-US').format(member.user.createdAt);
                 const memberAge = DiffDays(Date.parse(memberCreationDate), Date.parse(todayDate));
-                if (memberAge <= 3) {
-                    const embed = new MessageEmbed()
-                        .setAuthor(`User banned from ${client.user.username}`, client.user.displayAvatarURL())
-                        .setFooter(`Sent ${new Date(message.createdTimestamp).toLocaleDateString()} at ${new Date(message.createdTimestamp).toLocaleTimeString()}`)
-                        .setColor(color)
-                        .addFields(
-                            { name: 'Target', value: `<@${member.id}>` },
-                            { name: 'Reason', value: `L'account non rispetta i 3 giorni di registrazione minimi richiesti.` }
-                        )
 
+                if (memberAge <= 3) {
                     console.log(`Attivazione misure di sicurezza sull'account ${member.user.username} - ${member.id}`);
                     member.ban({
                         reason: "L'account non rispetta i 3 giorni di registrazione minimi richiesti. Aila."
                     }).then(() => {
                         if (log_channel != null || log_channel != undefined) {
+                            const embed = new MessageEmbed()
+                                .setAuthor(`User banned from ${client.user.username}`, client.user.displayAvatarURL())
+                                .setFooter(`Sent ${new Date(message.createdTimestamp).toLocaleDateString()} at ${new Date(message.createdTimestamp).toLocaleTimeString()}`)
+                                .setColor(color)
+                                .addFields(
+                                    { name: 'Target', value: `<@${member.id}>` },
+                                    { name: 'Reason', value: `L'account non rispetta i 3 giorni di registrazione minimi richiesti.` }
+                                )
                             const logChannel = member.guild.channels.cache.get(log_channel);
+
                             logChannel.send(embed);
                         }
                     });
