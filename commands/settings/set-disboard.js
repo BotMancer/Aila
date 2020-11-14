fdb67e00939f0d2e414f86a97f5c388be6d18ede
Luca Biagetti <notciabaku@gmail.com>
2020-11-14 01:42:04 +0100

    [CR] settings section, code refactoring before release.

--- commands/settings/set-disboard.js
+++ commands/settings/set-disboard.js
@@ -6,8 +6,9 @@ module.exports = {
     minArgs: 1,
     maxArgs: 3,
     callback: async (message, arguments, text, client, prefix, traslations) => {
-        const userState = arguments[0];
-        const guild = message.guild.id;
+        const userState = arguments[0]; //User input for state.
+        const guild = message.guild.id; //Current guild.
+
         //validate user input for state
         if (userState != 'enabled' && userState != 'disabled') {
             message.channel.send(`\`${userState}\` ` + traslations.error);
@@ -17,12 +18,14 @@ module.exports = {
             if (userState === 'enabled') return true;
             if (userState === 'disabled') return false;
         }
+
         //validate user input for #logchannel
         const log_channel = () => {
             if (message.mentions.channels.first() === undefined) return null;
             if (userState === 'disabled') return null;
             if (userState === 'enabled') return message.mentions.channels.first().id;
         }
+
         //validate user input for role to ping
         const role = () => {
             if (message.mentions.roles.first() === undefined) return null;
@@ -30,6 +33,7 @@ module.exports = {
             if (userState === 'enabled') return message.mentions.roles.first().id;
         }
 
+        //Updating DB document.
         await serverSettingsSchema.findByIdAndUpdate({
             _id: guild
         }, {
